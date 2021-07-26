03f8d3039b43dda2b2d2aaca0cd35390247106c2
sham843 <sham.jadhav@shauryatechnosoft.com>
2021-07-26 10:58:40 +0530

    commit

---
+++ src/app/services/call-api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CallAPIService {
+
+  constructor() { }
+}
