25f5b58749a6df40717de77e2c6c1057c6651f0b
Admin <Admin@20QTOKA>
2021-08-02 18:21:53 +0530

    commit

--- src/app/partial/members/view-members/view-members.component.ts
+++ src/app/partial/members/view-members/view-members.component.ts
@@ -1,4 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { CallAPIService } from 'src/app/services/call-api.service';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-view-members',
@@ -7,9 +14,152 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewMembersComponent implements OnInit {
   public items: string[] = [];
-  constructor() { }
+  allDistrict: any;
+  getTalkaByDistrict: any;
+  resultVillageOrCity: any;
+  resultAllViewMembers: any;
+  paginationNo: number = 1;
+  total: any;
+  pageSize: number = 10;
+  viewMembersObj:any = { DistrictId: 0, Talukaid: 0, villageid: 0, SearchText:''}
+  filterForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private callAPIService: CallAPIService,
+    private spinner: NgxSpinnerService,
+    private toastrService: ToastrService, private router: Router,
+    private commonService: CommonService, public datepipe: DatePipe,) { }
 
   ngOnInit(): void {
+    this.getViewMembers(this.viewMembersObj);
+    this.getDistrict();
+    this.defaultFilterForm();
   }
 
+  defaultFilterForm() {
+    this.filterForm = this.fb.group({
+      DistrictId: [''],
+      TalukaId: [''],
+      VillageId: [''],
+      searchText:['']
+    })
+  }
+
+  getDistrict() {
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_GetDistrict_1_0?StateId=' + 1, false, false, false, 'ncpServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.allDistrict = res.data1;
+      } else {
+        this.spinner.hide();
+        if (res.data == 1) {
+          this.toastrService.error("Data is not available 2");
+        } else {
+          this.toastrService.error("Please try again something went wrong");
+        }
+      }
+    })
+  }
+
+  getTaluka(districtId: any) {
+    this.viewMembersObj.DistrictId = districtId
+    this.getViewMembers(this.viewMembersObj);
+
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_GetTaluka_1_0?DistrictId=' + districtId, false, false, false, 'ncpServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.getTalkaByDistrict = res.data1;
+        console.log(this.getTalkaByDistrict)
+      } else {
+        this.spinner.hide();
+        if (res.data == 1) {
+          this.toastrService.error("Data is not available");
+        } else {
+          this.toastrService.error("Please try again something went wrong");
+        }
+      }
+    })
+  }
+
+  getVillageOrCity(talukaID: any) {
+    this.viewMembersObj.Talukaid = talukaID
+    this.getViewMembers(this.viewMembersObj);
+    
+    this.callAPIService.setHttp('get', 'Web_GetVillage_1_0?talukaid=' + talukaID, false, false, false, 'ncpServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.resultVillageOrCity = res.data1;
+
+      } else {
+        this.spinner.hide();
+        if (res.data == 1) {
+          this.toastrService.error("Data is not available1");
+        } else {
+          this.toastrService.error("Please try again something went wrong");
+        }
+      }
+    })
+  }
+
+  filterVillage(villageId: any){
+    this.viewMembersObj.villageid = villageId
+    this.getViewMembers(this.viewMembersObj);
+  }
+
+  getViewMembers(viewMembersObj: any) {
+    (viewMembersObj.DistrictId == undefined || viewMembersObj.DistrictId  == null) ? viewMembersObj.DistrictId = 0 :   viewMembersObj.DistrictId;  
+    (viewMembersObj.Talukaid == undefined || viewMembersObj.DistrictId  == null) ? viewMembersObj.Talukaid = 0 :   viewMembersObj.Talukaid;  
+    (viewMembersObj.villageid == undefined || viewMembersObj.DistrictId  == null) ? viewMembersObj.villageid = 0 :   viewMembersObj.villageid;  
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'ViewMembers_Web_1_0?UserId=' + this.commonService.loggedInUserId() + '&DistrictId=' + viewMembersObj.DistrictId + '&Talukaid=' + viewMembersObj.Talukaid + '&villageid=' + viewMembersObj.villageid + '&SearchText=' + viewMembersObj.SearchText + '&PageNo=' + this.paginationNo, false, false, false, 'ncpServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.resultAllViewMembers = res.data1;
+        this.total = res.data2[0].TotalCount;
+      } else {
+        this.spinner.hide();
+        if (res.data == 1) {
+          this.resultAllViewMembers = [];
+          this.toastrService.error("Data is not available");
+        } else {
+          this.toastrService.error("Please try again something went wrong");
+        }
+      }
+    })
+  }
+
+  onClickPagintion(pageNo: number) {
+    this.paginationNo = pageNo;
+    this.getViewMembers(this.viewMembersObj)
+  }
+
+  districtClear(flag: any) {
+    if (flag == 'district') {
+      this.viewMembersObj = { DistrictId: 0, Talukaid: 0, villageid: 0, SearchText: '' }
+      this.filterForm.reset();
+    } else if (flag == 'taluka') {
+      this.filterForm.reset({ DistrictId: this.viewMembersObj.DistrictId});
+      this.viewMembersObj = { 'DistrictId': this.viewMembersObj.DistrictId, 'TalukaId': this.filterForm.value.TalukaId, 'VillageId': this.filterForm.value.VillageId, SearchText:'' }
+    } 
+    else if (flag == 'village') {
+      this.filterForm.reset({
+        VillageId: 0
+      });
+      this.viewMembersObj = { 'DistrictId': this.viewMembersObj.DistrictId, 'TalukaId': this.filterForm.value.TalukaId, 'VillageId': this.filterForm.value.VillageId }
+    }else if (flag == 'search') {
+      this.viewMembersObj.SearchText = "";
+      this.filterForm.controls['searchText'].setValue('');
+    }
+    this.getViewMembers(this.viewMembersObj)
+  }
+
+  searchFilter(){
+    this.viewMembersObj.SearchText = this.filterForm.value.searchText
+    this.getViewMembers(this.viewMembersObj)
+  }
 }
--- src/app/partial/members/view-members/view-members.module.ts
+++ src/app/partial/members/view-members/view-members.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ViewMembersRoutingModule } from './view-members-routing.module';
 import { ViewMembersComponent } from './view-members.component';
 import { NgxSelectModule } from 'ngx-select-ex';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,11 @@ import { NgxSelectModule } from 'ngx-select-ex';
   imports: [
     CommonModule,
     ViewMembersRoutingModule,
-    NgxSelectModule
+    NgxSelectModule,
+    NgxSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule,
   ]
 })
 export class ViewMembersModule { }
