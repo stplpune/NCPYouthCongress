ca5f6d0663dafb8f392611887270720d4a410456
Admin <Admin@20QTOKA>
2021-08-02 12:15:46 +0530

    commit

--- src/app/partial/masters/organization-master/organization-details/organization-details.module.ts
+++ src/app/partial/masters/organization-master/organization-details/organization-details.module.ts
@@ -5,7 +5,7 @@ import { OrganizationDetailsComponent } from './organization-details.component';
 import { NgxSelectModule } from 'ngx-select-ex';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
-
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
     OrganizationDetailsComponent
@@ -16,7 +16,12 @@ import { NgxPaginationModule } from 'ngx-pagination';
     NgxSelectModule,
     ReactiveFormsModule,
     FormsModule,
-    NgxPaginationModule
-  ]
+    NgxPaginationModule,
+  AgmCoreModule.forRoot({
+    apiKey: 'AIzaSyBCSDtf8g7XZ9B-P20ZqzOIr1TUQAg4Fj0',
+    language: 'en',
+    libraries: ['places']
+  }),
+],
 })
 export class OrganizationDetailsModule { }
