37ed0a87c561f39430ee0d885e166a41a73e395d
Admin <Admin@20QTOKA>
2021-08-03 10:22:11 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { filter, map } from 'rxjs/operators';
+import { Subject } from "rxjs";
 import { Router, NavigationEnd, ActivatedRoute, NavigationStart } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ConnectionService } from 'ng-connection-service';
--- src/app/auth/authorization.guard.ts
+++ src/app/auth/authorization.guard.ts
@@ -8,6 +8,8 @@ import {
   CanActivateChild
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Subject } from "rxjs";
+
 // import { Observable } from 'rxjs';
 import { AuthorizationService } from './authorization.service';
 
