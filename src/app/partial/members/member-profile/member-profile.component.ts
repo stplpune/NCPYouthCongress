008ed05fc20d5c754fe75d121c3a092bd9012fd7
Admin <Admin@20QTOKA>
2021-08-10 12:36:26 +0530

    commit

--- src/app/partial/members/member-profile/member-profile.component.ts
+++ src/app/partial/members/member-profile/member-profile.component.ts
@@ -30,6 +30,7 @@ export class MemberProfileComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.memberId = localStorage.getItem('memberId')
     this.getMemberprofile();
+    this.WorkDoneByYuvak();
   }
 
 
@@ -39,11 +40,11 @@ export class MemberProfileComponent implements OnInit, OnDestroy {
     this.callAPIService.getHttp().subscribe((res: any) => {
       if (res.data == 0) {
         this.spinner.hide();
-        this.allMemberprofile = res.data1;
+        this.allMemberprofile = res.data1[0];
       } else {
         this.spinner.hide();
         if (res.data == 1) {
-          this.toastrService.error("Data is not available");
+          // this.toastrService.error("Data is not available");
         } else {
           this.toastrService.error("Please try again something went wrong");
         }
@@ -56,86 +57,126 @@ export class MemberProfileComponent implements OnInit, OnDestroy {
   WorkDoneByYuvak() {
     am4core.ready(() => {
       am4core.useTheme(am4themes_animated);
-      am4core.useTheme(am4themes_material);
-      let chart = am4core.create('WorkDoneByYuvak', am4charts.XYChart)
-      chart.colors.step = 2;
-
-      chart.legend = new am4charts.Legend()
-      chart.legend.position = 'top'
-      chart.legend.paddingBottom = 10
-      chart.legend.labels.template.maxWidth = 20
-
-      let xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
-      xAxis.dataFields.category = 'DistrictName'
-      xAxis.renderer.cellStartLocation = 0.1
-      xAxis.renderer.cellEndLocation = 0.9
-      xAxis.renderer.grid.template.location = 0;
-
-      let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      yAxis.min = 0;
-
-      function createSeries(value: string | undefined, name: string) {
-        let series = chart.series.push(new am4charts.ColumnSeries())
-        series.dataFields.valueY = value
-        series.dataFields.categoryX = 'DistrictName'
-        series.name = name
-
-        series.events.on("hidden", arrangeColumns);
-        series.events.on("shown", arrangeColumns);
-
-        let bullet = series.bullets.push(new am4charts.LabelBullet())
-        bullet.interactionsEnabled = false
-        bullet.dy = 30;
-        bullet.label.text = '{valueY}'
-        bullet.label.fill = am4core.color('#ffffff')
-
-        return series;
-      }
-
-      chart.data = this.recentActivityGraph;
-
-      chart.padding(10, 5, 5, 5);
-      createSeries('MemberWork', 'Work Done by Committes');
-      createSeries('TotalWork', 'Total Work Done');
-
-      function arrangeColumns() {
-
-        var series: any = chart.series.getIndex(0);
-
-        let w = 1 - xAxis.renderer.cellStartLocation - (1 - xAxis.renderer.cellEndLocation);
-        if (series.dataItems.length > 1) {
-          let x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
-          let x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
-          let delta = ((x1 - x0) / chart.series.length) * w;
-          if (am4core.isNumber(delta)) {
-            let middle = chart.series.length / 2;
-
-            let newIndex = 0;
-            chart.series.each(function (series) {
-              if (!series.isHidden && !series.isHiding) {
-                series.dummyData = newIndex;
-                newIndex++;
-              }
-              else {
-                series.dummyData = chart.series.indexOf(series);
-              }
-            })
-            let visibleCount = newIndex;
-            let newMiddle = visibleCount / 2;
-
-            chart.series.each(function (series) {
-              let trueIndex = chart.series.indexOf(series);
-              let newIndex = series.dummyData;
-
-              let dx = (newIndex - trueIndex + middle - newMiddle) * delta
-
-              series.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
-              series.bulletsContainer.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
-            })
-          }
-        }
-      }
-
+      // Themes end
+      
+      // Create chart instance
+      let chart = am4core.create("recentActivityGraph", am4charts.XYChart);
+      
+      // Enable chart cursor
+      chart.cursor = new am4charts.XYCursor();
+      chart.cursor.lineX.disabled = true;
+      chart.cursor.lineY.disabled = true;
+      
+      // Enable scrollbar
+      // chart.scrollbarX = new am4core.Scrollbar();
+      
+      // Add data
+      chart.data = [{
+            "date": "2012-01-01",
+            "value": 0
+        }, {
+            "date": "2012-01-02",
+            "value": 1
+        }, {
+            "date": "2012-01-03",
+            "value": 2
+        }, {
+            "date": "2012-01-04",
+            "value": 3
+        }, {
+            "date": "2012-01-05",
+            "value": 4
+        }, {
+            "date": "2012-01-06",
+            "value": 6
+        }, {
+            "date": "2012-01-07",
+            "value": 7
+        }, {
+            "date": "2012-01-08",
+            "value": 5
+        },  {
+            "date": "2012-01-21",
+            "value": 8
+        }, {
+            "date": "2012-01-22",
+            "value": 9
+        }, {
+            "date": "2012-01-23",
+            "value": 10
+        }, {
+          "date": "2012-01-21",
+          "value": 11
+      }, {
+          "date": "2012-01-22",
+          "value": 12
+      }, ];
+      
+      // Create axes
+      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0.5;
+      dateAxis.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+      dateAxis.renderer.minGridDistance = 40;
+      dateAxis.tooltipDateFormat = "MMM dd, yyyy";
+      dateAxis.dateFormats.setKey("day", "dd");
+      
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      
+      // Create series
+      let series:any = chart.series.push(new am4charts.LineSeries());
+      series.tooltipText = "{date}\n[bold font-size: 17px]value: {valueY}[/]";
+      series.dataFields.valueY = "value";
+      series.dataFields.dateX = "date";
+      series.strokeDasharray = 3;
+      series.strokeWidth = 2
+      series.strokeOpacity = 0.3;
+      series.strokeDasharray = "3,3"
+      
+      let bullet = series.bullets.push(new am4charts.CircleBullet());
+      bullet.strokeWidth = 2;
+      bullet.stroke = am4core.color("#fff");
+      bullet.setStateOnChildren = true;
+      bullet.propertyFields.fillOpacity = "opacity";
+      bullet.propertyFields.strokeOpacity = "opacity";
+      
+      let hoverState = bullet.states.create("hover");
+      hoverState.properties.scale = 1.7;
+      
+      function createTrendLine(data:any) {
+        let trend = chart.series.push(new am4charts.LineSeries());
+        trend.dataFields.valueY = "value";
+        trend.dataFields.dateX = "date";
+        trend.strokeWidth = 2
+        trend.stroke = trend.fill = am4core.color("#c00");
+        trend.data = data;
+      
+        let bullet = trend.bullets.push(new am4charts.CircleBullet());
+        bullet.tooltipText = "{date}\n[bold font-size: 17px]value: {valueY}[/]";
+        bullet.strokeWidth = 2;
+        bullet.stroke = am4core.color("#fff")
+        bullet.circle.fill = trend.stroke;
+      
+        let hoverState = bullet.states.create("hover");
+        hoverState.properties.scale = 1.7;
+      
+        return trend;
+      };
+      
+      // createTrendLine([
+      //   { "date": "2012-01-02", "value": 10 },
+      //   { "date": "2012-01-11", "value": 19 }
+      // ]);
+      
+      // let lastTrend = createTrendLine([
+      //   { "date": "2012-01-17", "value": 16 },
+      //   { "date": "2012-01-22", "value": 10 }
+      // ]);
+      
+      // Initial zoom once chart is ready
+      // lastTrend.events.once("datavalidated", function(){
+      //   series.xAxis.zoomToDates(new Date(2012, 0, 2), new Date(2012, 0, 13));
+      // });
+      
     });
   }
 
