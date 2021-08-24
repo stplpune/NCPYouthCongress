4e57a4a1f3babc883c8378c6a78d6ef8fabda6a4
Admin <Admin@20QTOKA>
2021-08-24 15:22:26 +0530

    add cdk

--- src/app/partial/masters/event-master/event-master.module.ts
+++ src/app/partial/masters/event-master/event-master.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AngularEditorModule } from '@kolkov/angular-editor';
 import { EventMasterRoutingModule } from './event-master-routing.module';
 import { EventMasterComponent } from './event-master.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,10 @@ import { EventMasterComponent } from './event-master.component';
   ],
   imports: [
     CommonModule,
-    EventMasterRoutingModule
+    EventMasterRoutingModule,
+    AngularEditorModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class EventMasterModule { }
