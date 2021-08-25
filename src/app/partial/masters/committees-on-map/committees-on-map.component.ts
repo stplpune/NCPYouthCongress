18dca06c4b67023ba580fb1021c40a342682db3a
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-08-25 19:26:26 +0530

    committees on map page created.

--- src/app/partial/masters/committees-on-map/committees-on-map.component.ts
+++ src/app/partial/masters/committees-on-map/committees-on-map.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-committees-on-map',
   templateUrl: './committees-on-map.component.html',
-  styleUrls: ['./committees-on-map.component.css']
+  styleUrls: ['./committees-on-map.component.css', '../../partial.component.css']
 })
 export class CommitteesOnMapComponent implements OnInit {
 
