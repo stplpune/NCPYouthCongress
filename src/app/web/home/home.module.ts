14d9e5f92a044b813781ac030f3cb176e1539168
Admin <Admin@20QTOKA>
2021-08-25 19:35:58 +0530

    commit

--- src/app/web/home/home.module.ts
+++ src/app/web/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ToastrModule } from 'ngx-toastr';
+
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ToastrModule,
+
   ]
 })
 export class HomeModule { }
