cbd7e17602418bf756fb536bd2bb795e34cc0a39
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2021-09-16 10:31:26 +0530

    commit

--- src/app/partial/election/create-election/create-election.component.ts
+++ src/app/partial/election/create-election/create-election.component.ts
@@ -4,6 +4,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { CallAPIService } from 'src/app/services/call-api.service';
 import { CommonService } from 'src/app/services/common.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-election',
@@ -16,6 +17,7 @@ export class CreateElectionComponent implements OnInit {
   submitted = false;
   boothListTypeArray = [{ id: 1, name: "Assembly Booth List" }, { id: 2, name: "User Defined Booth List" }];
   subElectionAppArray = [{ id: 1, name: "Yes" }, { id: 2, name: "No" }];
+  webInsertEleMasterArray: any;
 
   constructor(
     private spinner: NgxSpinnerService,
@@ -23,10 +25,13 @@ export class CreateElectionComponent implements OnInit {
     private toastrService: ToastrService,
     private fb: FormBuilder,
     private commonService: CommonService,
+    private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
     this.defaultProgramForm();
+   // this.getWebInsertElectionMaster();
   }
 
   defaultProgramForm() {
@@ -50,5 +55,24 @@ export class CreateElectionComponent implements OnInit {
     this.submitted = false;
     this.defaultProgramForm();
   }
+
+  // getWebInsertElectionMaster() {
+  //   this.spinner.show();
+
+  //   let obj =  + 'ElectionName=' +  + '&ElectionTypeId=' +  + '&IsSubElectionApplicable=' +  + '&IsAsemblyBoothListApplicable=' +  + '&CreatedBy=' +  + '&StrSubElectionId=' + ;
+  //   this.callAPIService.setHttp('get', 'Web_Insert_ElectionMaster?Id=' + obj, false, false, false, 'ncpServiceForWeb');
+  //   this.callAPIService.getHttp().subscribe((res: any) => {
+  //     if (res.data == 0) {
+  //       this.spinner.hide();
+  //       this.webInsertEleMasterArray = res.data1;
+  //     } else {
+  //       this.toastrService.error("Data is not available");
+  //     }
+  //   }, (error: any) => {
+  //     if (error.status == 500) {
+  //       this.router.navigate(['../500'], { relativeTo: this.route });
+  //     }
+  //   })
+  // }
 
 }
