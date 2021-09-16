43a8bb6719481ae62dd7a7833b6e81eae9a14cd5
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-09-16 19:04:48 +0530

    election profile page design

--- src/app/partial/election/election-profile/election-profile.component.ts
+++ src/app/partial/election/election-profile/election-profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-election-profile',
   templateUrl: './election-profile.component.html',
-  styleUrls: ['./election-profile.component.css']
+  styleUrls: ['./election-profile.component.css', '../../partial.component.css']
 })
 export class ElectionProfileComponent implements OnInit {
 
