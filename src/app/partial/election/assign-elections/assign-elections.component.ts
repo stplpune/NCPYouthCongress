3ba18659e9d116a9c6d3ae9b14ad213d09ee1d8b
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-09-30 17:47:05 +0530

    Add elections to client page design.

--- src/app/partial/election/assign-elections/assign-elections.component.ts
+++ src/app/partial/election/assign-elections/assign-elections.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-assign-elections',
   templateUrl: './assign-elections.component.html',
-  styleUrls: ['./assign-elections.component.css']
+  styleUrls: ['./assign-elections.component.css', '../../partial.component.css']
 })
 export class AssignElectionsComponent implements OnInit {
 
