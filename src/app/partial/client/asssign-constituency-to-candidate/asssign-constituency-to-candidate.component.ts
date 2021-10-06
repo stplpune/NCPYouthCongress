362c589857410b987b972426b17e8caf9c382a19
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-10-06 18:59:30 +0530

    assign constituency to candidate page design completed.

--- src/app/partial/client/asssign-constituency-to-candidate/asssign-constituency-to-candidate.component.ts
+++ src/app/partial/client/asssign-constituency-to-candidate/asssign-constituency-to-candidate.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-asssign-constituency-to-candidate',
   templateUrl: './asssign-constituency-to-candidate.component.html',
-  styleUrls: ['./asssign-constituency-to-candidate.component.css']
+  styleUrls: ['./asssign-constituency-to-candidate.component.css', '../../partial.component.css']
 })
 export class AsssignConstituencyToCandidateComponent implements OnInit {
 
