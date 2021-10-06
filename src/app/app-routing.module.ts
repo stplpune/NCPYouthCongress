1dfacef30ea2af6669289132bde29c70bf7fb5eb
sham <sham.jadhav@shauryatechnosoft.com>
2021-10-06 16:09:16 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -61,7 +61,7 @@ const routes: Routes = [
       { path: 'add-member', loadChildren: () => import('./partial/masters/add-member/add-member.module').then(m => m.AddMemberModule), data: { title: 'Dashboard', allowedRoles: ['7', '1'] }  },
       { path: 'assign-elections', loadChildren: () => import('./partial/election/assign-elections/assign-elections.module').then(m => m.AssignElectionsModule), data: { title: 'Assign Election to Client', allowedRoles: ['7', '1'] } },
       { path: 'candidate-registration', loadChildren: () => import('./partial/election/candidate-registration/candidate-registration.module').then(m => m.CandidateRegistrationModule), data: { title: 'Candidate Registration', allowedRoles: ['7', '1'] } }, 
-      { path: 'asssign-constituency-to-candidate', loadChildren: () => import('./partial/client/asssign-constituency-to-candidate/asssign-constituency-to-candidate.module').then(m => m.AsssignConstituencyToCandidateModule), data: { title: 'Asssign Constituency To Candidate', allowedRoles: ['7', '1'] } }, 
+     
   
       // Client
       { path: 'assign-agents-to-booths', loadChildren: () => import('./partial/client/assign-agents-to-booths/assign-agents-to-booths.module').then(m => m.AssignAgentsToBoothsModule), data: { title: 'Assign Voters', allowedRoles: ['8', '1'] } },
@@ -69,7 +69,7 @@ const routes: Routes = [
       { path: 'c-dashboard', loadChildren: () => import('./partial/client/c-dashboard/c-dashboard.module').then(m => m.CDashboardModule), data: { title: 'Dashboard', allowedRoles: ['8', '1'] } },
       { path: 'election-details', loadChildren: () => import('./partial/client/election-details/election-details.module').then(m => m.ElectionDetailsModule), data: { title: 'Election Details', allowedRoles: ['8', '1'] } },
       { path: 'view-booth-voters', loadChildren: () => import('./partial/client/view-booth-voters/view-booth-voters.module').then(m => m.ViewBoothVotersModule), data: { title: 'View Booth Voters', allowedRoles: ['8', '1'] } },
-
+      { path: 'asssign-constituency-to-candidate', loadChildren: () => import('./partial/client/asssign-constituency-to-candidate/asssign-constituency-to-candidate.module').then(m => m.AsssignConstituencyToCandidateModule), data: { title: 'Asssign Constituency To Candidate', allowedRoles: ['8', '1'] } }, 
       { path: 'access-denied', component: AccessDeniedComponent },
     ]
   },
