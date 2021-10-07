c02bba351ad336b34c7ddba8230bae9ecc59bebd
sham <sham.jadhav@shauryatechnosoft.com>
2021-10-07 14:41:07 +0530

    add member page add table and filter

--- src/app/partial/masters/add-member/add-member.module.ts
+++ src/app/partial/masters/add-member/add-member.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AddMemberRoutingModule } from './add-member-routing.module';
 import { AddMemberComponent } from './add-member.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxSelectModule } from 'ngx-select-ex';
+import { MyProfileRoutingModule } from '../../user-profile/my-profile/my-profile-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 @NgModule({
@@ -11,7 +15,12 @@ import { AddMemberComponent } from './add-member.component';
   ],
   imports: [
     CommonModule,
-    AddMemberRoutingModule
+    AddMemberRoutingModule,
+    MyProfileRoutingModule,
+    NgxSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule
   ]
 })
 export class AddMemberModule { }
