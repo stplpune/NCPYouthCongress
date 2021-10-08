5164c059c8c3ea9ecdd94fda238745b400c1c179
sham <sham.jadhav@shauryatechnosoft.com>
2021-10-08 19:22:24 +0530

    org details page changes

--- src/app/partial/members/member-report/member-report.component.ts
+++ src/app/partial/members/member-report/member-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any
 @Component({
   selector: 'app-member-report',
   templateUrl: './member-report.component.html',
@@ -11,4 +11,22 @@ export class MemberReportComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  printDiv(el:any) {
+    var restorepage = $('body').html();
+    var printcontent = $('#' + el).clone();
+    $('body').empty().html(printcontent);
+    window.print();
+    $('body').html(restorepage);
+
+    // var divContents:any = document.getElementById("printarea")
+    // divContents.innerHTML;
+    // var a:any = window.open('', '', 'height=500, width=500');
+    // a.document.write('<html>');
+    // a.document.write('<body > <h1>Div contents are <br>');
+    // a.document.write(divContents);
+    // a.document.write('</body></html>');
+    // a.document.close();
+    // a.print();
+}
 }
