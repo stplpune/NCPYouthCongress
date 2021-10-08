f8ffa2e003e6ea9c1f795108d23ae5fe3abdf83c
sham <sham.jadhav@shauryatechnosoft.com>
2021-10-08 19:48:30 +0530

    member report page changes

--- src/app/partial/members/member-report/member-report.component.ts
+++ src/app/partial/members/member-report/member-report.component.ts
@@ -1,32 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-declare var $: any
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+declare var $: any;
+// import { jsPDF } from "jspdf";
+
 @Component({
   selector: 'app-member-report',
   templateUrl: './member-report.component.html',
   styleUrls: ['./member-report.component.css', '../../partial.component.css']
 })
 export class MemberReportComponent implements OnInit {
-
+  @ViewChild('pdfTable', {static: false}) pdfTable!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  printDiv(el:any) {
-    var restorepage = $('body').html();
-    var printcontent = $('#' + el).clone();
-    $('body').empty().html(printcontent);
+  printPage() {
     window.print();
-    $('body').html(restorepage);
+  }
 
-    // var divContents:any = document.getElementById("printarea")
-    // divContents.innerHTML;
-    // var a:any = window.open('', '', 'height=500, width=500');
-    // a.document.write('<html>');
-    // a.document.write('<body > <h1>Div contents are <br>');
-    // a.document.write(divContents);
-    // a.document.write('</body></html>');
-    // a.document.close();
-    // a.print();
-}
+  downloadPdf() {
+    // let doc:any = new jsPDF();
+    // const specialElementHandlers = {
+    //   '#editor': (element:any, renderer:any)=>{
+    //     return true;
+    //   }
+    // };
+
+    // const pdfTable = this.pdfTable.nativeElement;
+
+    // doc.fromHTML(pdfTable.innerHTML, 15, 15, {
+    //   width: 190,
+    //   'elementHandlers': specialElementHandlers
+    // });
+
+    // doc.save('tableToPdf.pdf');
+  }
 }
