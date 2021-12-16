ff86b65bea20b4029aecae345f1034ace4b41818
sham <sham.jadhav@shauryatechnosoft.com>
2021-12-16 12:07:59 +0530

    Validators.maxLength

--- src/app/partial/dialogs/add-committee/add-committee.component.ts
+++ src/app/partial/dialogs/add-committee/add-committee.component.ts
@@ -62,7 +62,7 @@ export class AddCommitteeComponent implements OnInit {
   customForm() {
     this.addCommitteeForm = this.fb.group({
       Id:[0],
-      BodyOrgCellName: ['', [Validators.required,Validators.maxLength(50)]],
+      BodyOrgCellName: ['', [Validators.required,Validators.maxLength(100)]],
       StateId: ['', Validators.required],
       DistrictId: [],
       TalukaId: [''],
